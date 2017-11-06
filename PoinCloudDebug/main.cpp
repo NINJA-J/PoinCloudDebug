d610fc47b44135f391d9144629a3394289555f88
NINJA-J <1605563934@qq.com>
2017-11-06 13:35:55 +0800

    Initial Commit

---
+++ PoinCloudDebug/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PoinCloudDebug
+//
+//  Created by Jonathan闫 on 2017/11/6.
+//  Copyright © 2017年 Jonathan闫. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
